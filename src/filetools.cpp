59a9414e412d6697b77f23aa74891cde173ca7d2
Ricardo Oliveira <ricardo-cadastros@hotmail.com>
2021-10-15 21:42:56 -0300

    [feature] Utiliza o CUDA.

--- src/filetools.cpp
+++ src/filetools.cpp
@@ -6,6 +6,7 @@
 #include "CImg.h"
 #include "PPMReader.h"
 #include "PPMImage.h"
+#include <cuda.h>
 
 using namespace std;
 
@@ -18,9 +19,10 @@ using namespace std;
  *
  * returns: inline bool
  */
-bool has_file (const char * path) {
+bool has_file(const char *path)
+{
   struct stat buffer;
-  return (stat (path, &buffer) == 0);
+  return (stat(path, &buffer) == 0);
 }
 
 /**
@@ -32,18 +34,20 @@ bool has_file (const char * path) {
  *
  * returns: void
  */
-char * to_ppm(const char * path) {
+char *to_ppm(const char *path)
+{
   // Gera o nome do arquivo
-  char * filename = get_filename(path);
-  filename        = strcat(filename, ".ppm");
-  char * fullpath = new char[strlen(filename) + sizeof(char) * 5];
+  char *filename = get_filename(path);
+  filename = strcat(filename, ".ppm");
+  char *fullpath = new char[strlen(filename) + sizeof(char) * 5];
   strcpy(fullpath, "img/");
   strcat(fullpath, filename);
-  char * c_random;
+  char *c_random;
   string random;
 
   // Verifica se o nome do arquivo já existe
-  while (has_file(fullpath)) {
+  while (has_file(fullpath))
+  {
     // Gera o novo nome
     filename = get_filename(path);
     random = to_string(rand() % 1000);
@@ -73,7 +77,8 @@ char * to_ppm(const char * path) {
  *
  * returns: char *
  */
-char * get_filename(const char * path) {
+char *get_filename(const char *path)
+{
   string cpp_path = string(path);
   size_t last_bar = cpp_path.find_last_of("/");
   size_t last_dot = cpp_path.find_last_of(".");
@@ -91,23 +96,25 @@ char * get_filename(const char * path) {
  *
  * returns: int *
  */
-int * extract_effects(const char * effects) {
-  const char * delim = ",";
-  char * copy        = new char[strlen(effects) + 1]; // Cópia da string effects para ser manipulada
-  char * token       = new char[strlen(effects) + 1]; // Token, i.e. efeitos
-  char * sanitized   = new char[strlen(token) + 1];   // Token sanitizado
-  int * effects_arr  = (int *) malloc(0);             // Reserva espaço na memória para o vetor de efeitos
+int *extract_effects(const char *effects)
+{
+  const char *delim = ",";
+  char *copy = new char[strlen(effects) + 1];    // Cópia da string effects para ser manipulada
+  char *token = new char[strlen(effects) + 1];   // Token, i.e. efeitos
+  char *sanitized = new char[strlen(token) + 1]; // Token sanitizado
+  int *effects_arr = (int *)malloc(0);           // Reserva espaço na memória para o vetor de efeitos
   size_t size = 0;
 
   // Extrai os efeitos da string copy
   strcpy(copy, effects);
   token = strtok(copy, delim);
-  while(token != NULL) {
+  while (token != NULL)
+  {
     strcpy(sanitized, token);
 
     // Realoca o tamanho do vetor e insere o novo elemento
     size += sizeof(int);
-    effects_arr = (int *) realloc(effects_arr, size);
+    effects_arr = (int *)realloc(effects_arr, size);
     effects_arr[(int)(size / sizeof(int)) - 1] = atoi(trim(sanitized));
 
     // Busca pelo próximo token
@@ -116,7 +123,7 @@ int * extract_effects(const char * effects) {
 
   // Realoca o vetor para indicar seu fim
   size += sizeof(int);
-  effects_arr = (int *) realloc(effects_arr, size);
+  effects_arr = (int *)realloc(effects_arr, size);
   effects_arr[(int)(size / sizeof(int)) - 1] = INT_MIN;
 
   return effects_arr;
@@ -131,11 +138,12 @@ int * extract_effects(const char * effects) {
  *
  * returns: char *
  */
-char * ltrim(char * str) {
-  while(isspace(*str)) str++;
+char *ltrim(char *str)
+{
+  while (isspace(*str))
+    str++;
   return str;
 }
-
 
 /**
  * Function: rtrim
@@ -146,9 +154,11 @@ char * ltrim(char * str) {
  *
  * returns: char *
  */
-char * rtrim(char * str) {
-  char * back = str + strlen(str);
-  while(isspace(*--back));
+char *rtrim(char *str)
+{
+  char *back = str + strlen(str);
+  while (isspace(*--back))
+    ;
   *(back + 1) = '\0';
   return str;
 }
@@ -162,7 +172,8 @@ char * rtrim(char * str) {
  *
  * returns: char *
  */
-char * trim(char * str) {
+char *trim(char *str)
+{
   return rtrim(ltrim(str));
 }
 
@@ -175,79 +186,97 @@ char * trim(char * str) {
  *
  * returns: void
  */
-void apply_effects(const char * path, int * effects) {
+void apply_effects(const char *path, int *effects)
+{
   // Setta o máximo de threads disponíveis
   omp_set_num_threads(omp_get_max_threads());
 
   // Lê a imagem ppm gerada
   PPMReader reader(path);
-  char * filename = (char *) malloc(0);
-  char * fullpath = (char *) malloc(0);
+  char *filename = (char *)malloc(0);
+  char *fullpath = (char *)malloc(0);
 
   // Aplica os efeitos
-  while (*effects != INT_MIN) {
-    switch(*effects) {
-      case 1:
-        reader.load();
-        reverseColor(reader.getImage());
-        fullpath = read_str("img/");
-        filename = get_filename(path);
-        strcat(filename, "_1.ppm");
-        strcat(fullpath, filename);
-        reader.write(fullpath);
-        break;
-      case 2:
-        reader.load();
-        green(reader.getImage());
-        fullpath = read_str("img/");
-        filename = get_filename(path);
-        strcat(filename, "_2.ppm");
-        strcat(fullpath, filename);
-        reader.write(fullpath);
-        break;
-      case 3:
-        reader.load();
-        striped(reader.getImage());
-        fullpath = read_str("img/");
-        filename = get_filename(path);
-        strcat(filename, "_3.ppm");
-        strcat(fullpath, filename);
-        reader.write(fullpath);
-        break;
-      default:
-        cout << "O efeito '" << *effects << "' não foi encontrado." << endl;
+  while (*effects != INT_MIN)
+  {
+    switch (*effects)
+    {
+    case 1:
+      reader.load();
+      reverseColor(reader.getImage());
+      fullpath = read_str("img/");
+      filename = get_filename(path);
+      strcat(filename, "_1.ppm");
+      strcat(fullpath, filename);
+      reader.write(fullpath);
+      break;
+    case 2:
+      reader.load();
+      green(reader.getImage());
+      fullpath = read_str("img/");
+      filename = get_filename(path);
+      strcat(filename, "_2.ppm");
+      strcat(fullpath, filename);
+      reader.write(fullpath);
+      break;
+    case 3:
+      reader.load();
+      striped(reader.getImage());
+      fullpath = read_str("img/");
+      filename = get_filename(path);
+      strcat(filename, "_3.ppm");
+      strcat(fullpath, filename);
+      reader.write(fullpath);
+      break;
+    default:
+      cout << "O efeito '" << *effects << "' não foi encontrado." << endl;
     }
 
     // Para o otimizador do compilador não achar que essa variável não serve para nada
-    if (*(effects++)) {};
+    if (*(effects++))
+    {
+    }
   }
 
   free(fullpath);
   free(filename);
 }
 
-void reverseColor(PPMImage * img) {
-  #pragma omp parallel for
-  for (int i = 0; i < img->width * img->height; i++) {
+void reverseColor(PPMImage *img)
+{
+  int size = img->width * img->height * sizeof(PPMPixel *) + sizeof(img);
+  PPMImage *cpy;
+  cudaMalloc((void **)&cpy, size);
+  cudaMemcpy(cpy, img, size, cudaMemcpyHostToDevice);
+  // #pragma omp parallel for
+  for (int i = 0; i < img->width * img->height; i++)
+  {
     img->pixel[i].r = img->maxColorVal - img->pixel[i].r;
     img->pixel[i].g = img->maxColorVal - img->pixel[i].g;
     img->pixel[i].b = img->maxColorVal - img->pixel[i].b;
   }
+
+  cudaFree(cpy);
 }
 
-void green(PPMImage * img) {
-  #pragma omp parallel for
-  for (int i = 0; i < img->width * img->height; i++) {
-    img->pixel[i].r = (int) img->pixel[i].r * 0.2126;
-    img->pixel[i].g = (int) img->pixel[i].g * 0.7152;
-    img->pixel[i].b = (int) img->pixel[i].b * 0.0722;
+void green(PPMImage *img)
+{
+#pragma omp parallel for
+  for (int i = 0; i < img->width * img->height; i++)
+  {
+    img->pixel[i].r = (int)img->pixel[i].r * 0.2126;
+    img->pixel[i].g = (int)img->pixel[i].g * 0.7152;
+    img->pixel[i].b = (int)img->pixel[i].b * 0.0722;
   }
 }
 
-void striped(PPMImage * img) {
-  #pragma omp parallel for
-  for (int i = 0; i < img->width * img->height; i++) {
-    if (i % 3 == 0) {
+void striped(PPMImage *img)
+{
+#pragma omp parallel for
+  for (int i = 0; i < img->width * img->height; i++)
+  {
+    if (i % 3 == 0)
+    {
       img->pixel[i].r = img->maxColorVal - img->pixel[i].r;
       img->pixel[i].g = img->maxColorVal - img->pixel[i].g;
       img->pixel[i].b = img->maxColorVal - img->pixel[i].b;
